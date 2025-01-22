059497d39ae6c007515b4301e099613ae1388313
Tarcisio Da Silva Bueno <bueno-23@lame23.enst.fr>
2025-01-22 16:31:12 +0100

    added predict_single_tree_score function

--- streamRHFv5withPredict.py
+++ streamRHFv5withPredict.py
@@ -312,6 +312,19 @@ class RHF(object):
 		self.scores = np.zeros(window_size)
 		self.number_instances = 0
 
+	def predict_single_tree_score(self, tree, node, tree_index, xi, xi_index):
+		if node.type ==	1:
+			if node.has_duplicates:					
+				p = (1+node.uniques_)/self.number_instances
+				return np.log(1/(p))
+			else:				
+				p = (1+node.size)/self.number_instances
+				return np.log(1/(p))
+		elif xi[0, node.attribute] <= node.value:
+			return self.predict_single_tree_score(tree, node.left, tree_index, xi, xi_index)
+		else:
+			return self.predict_single_tree_score(tree, node.right, tree_index, xi, xi_index)
+  
 	def insert(self, tree, node, tree_index, xi, xi_index):
 		new_data = np.vstack([node.data, xi])
 		new_indices = np.append(node.data_indices, xi_index)
@@ -505,6 +518,13 @@ def main(dataset_name: str, shuffle: bool=True):
 			# with list comprehension
 			# [my_rhf.insert(tree, tree.tree_, tree.index, new_instance) for tree_i, tree in enumerate(my_rhf.forest)]
 			
+			# This is the predict part
+			predicted_score = 0
+			for tree_i, tree in enumerate(my_rhf.forest):
+				predicted_score += my_rhf.predict_single_tree_score(tree, tree.tree_, tree.index, new_instance, i)
+			print(predicted_score)
+			# This is the predict part
+   
 			for tree_i, tree in enumerate(my_rhf.forest):				
 				my_rhf.insert(tree, tree.tree_, tree.index, new_instance, i)
 				# delete and add old and new leaves to the tree.leaves list
