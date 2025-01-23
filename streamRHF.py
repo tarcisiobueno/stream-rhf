81b9688f6a36388b641b05a17851243c4f436b05
Tarcisio Da Silva Bueno <bueno-23@lame25.enst.fr>
2025-01-24 00:23:32 +0100

    instructions to run the file

--- streamRHF.py
+++ streamRHF.py
@@ -1,3 +1,27 @@
+"""
+Python implementation of StreamRHF algorithm for anomaly detection in data streams.
+The code is based on the following papers:
+
+STREamRHF: Tree-Based Unsupervised Anomaly Detection for Data Streams by Nesic, Stefan and Putina, Andrian and Bahri, Maroua and Huet, Alexis and Navarro, Jose Manuel and Rossi, Dario and Sozio, Mauro
+Random Histogram Forest for Unsupervised Anomaly Detection by Putina, Andrian and Sozio, Mauro and Rossi, Dario and Navarro, José Manuel
+
+The code was inspired by the official implementation of both papers which can be found at https://github.com/stefannesic/streamRHF and https://github.com/anrputina/rhf.
+
+To use it you need to have the following libraries installed:
+- numpy
+- pandas
+- scikit-learn
+- tqdm
+- joblib
+
+Have the data in ../data/public/. (You may change this path in the code)
+Have a folder ./results/ to save the results. (You may change this path in the code)
+
+to run the code use the following command:
+python streamRHF.py dataset1 dataset2 dataset3 ...
+
+where dataset1, dataset2, dataset3, ... are the names of the datasets to be processed. They shuld be in the ../data/public/ folder.
+"""
 from multiprocessing import Pool
 import os
 import sys
@@ -420,7 +444,7 @@ def process_dataset_iteration(args):
 	tic = time.time()
 	av_precision = main(data)
 	toc = time.time()
-	with open(f"./resultsStreamRHF_6/{dataset_name}.csv", "a") as f:
+	with open(f"./results/{dataset_name}.csv", "a") as f:
 		f.write(f"{iteration},{av_precision},{toc - tic}\n")
 		print(f"Completed {dataset_name} iteration {iteration} with score {av_precision}")
 	return dataset_name, iteration, av_precision, toc - tic
